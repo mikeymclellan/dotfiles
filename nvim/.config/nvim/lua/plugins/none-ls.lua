decbf1c30558a0017906d77721bfc9ed3f78b99f
mikeymclellan <mikey@mclellan.org.nz>
2024-11-14 16:54:58 +1300

    Some improvements to `doug`

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -6,13 +6,13 @@ return {
     config = function()
         local null_ls = require("null-ls")
         null_ls.setup({
-            debug = true,
+            -- debug = true,
             sources = {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.diagnostics.erb_lint,
                 null_ls.builtins.formatting.pretty_php,
-                require("none-ls.diagnostics.eslint_d"),
+                -- require("none-ls.diagnostics.eslint_d"),
             },
         })
 
