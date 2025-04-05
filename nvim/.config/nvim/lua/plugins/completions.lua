809b4ca41ae482553878ef3ee0c93b4e551241ab
mikeymclellan <mikey@mclellan.org.nz>
2025-04-06 07:29:11 +1200

    Add copilot

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -31,49 +31,81 @@ return {
 			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "copilot" },
-					{ name = "cmp-dbee" },
-					{ name = "luasnip" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
-	{
-		"zbirenbaum/copilot.lua",
-		event = "InsertEnter",
-		opts = {
-			-- Possible configurable fields can be found on:
-			-- https://github.com/zbirenbaum/copilot.lua#setup-and-configuration
-			suggestion = {
-				enable = false,
-			},
-			panel = {
-				enable = false,
-			},
-			filetypes = {
-				markdown = true,
-			},
-		},
-	},
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "copilot" },
+                    { name = "cmp-dbee" },
+                    { name = "luasnip" },
+                }, {
+                        { name = "buffer" },
+                    }),
+            })
+        end,
+    },
+    {
+        "zbirenbaum/copilot.lua",
+        event = "InsertEnter",
+        opts = {
+            -- Possible configurable fields can be found on:
+            -- https://github.com/zbirenbaum/copilot.lua#setup-and-configuration
+            suggestion = {
+                enable = false,
+            },
+            panel = {
+                enable = false,
+            },
+            filetypes = {
+                markdown = true,
+            },
+        },
+    },
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+            { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+        },
+        build = "make tiktoken", -- Only on MacOS or Linux
+        opts = {
+            window = {
+                layout = 'float',
+                relative = 'cursor',
+                width = 1,
+                height = 0.4,
+                row = 1
+            }
+            -- See Configuration section for options
+        },
+        config = function()
+            require("CopilotChat").setup({
+                window = {
+                    layout = 'float',
+                    relative = 'cursor',
+                    width = 1,
+                    height = 0.4,
+                    row = 1
+                }
+            })
+            vim.keymap.set("n", "<leader>cc", ":CopilotChatToggle <CR>", {})
+            vim.keymap.set("v", "<leader>cc", require("CopilotChat").open, {})
+        end
+        -- See Commands section for default commands if you want to lazy load on them
+    }
 }
