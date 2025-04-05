395732ed3c10027652dd73a43333fd1166ca7979
mikeymclellan <mikey@mclellan.org.nz>
2025-04-06 07:29:50 +1200

    Upgrade to nvim 0.11. Replace null-ls with Conform

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -1,9 +1,20 @@
 return {
-   'sindrets/diffview.nvim',
+    "sindrets/diffview.nvim",
     config = function()
-      require('diffview').setup()
-      vim.keymap.set("n", "<leader>dvh", ":DiffviewFileHistory %<CR>", {})
-      vim.keymap.set("n", "<leader>dv", ":DiffviewToggleFiles<CR>", {})
-
-   end,
+        local actions = require("diffview.actions");
+        require("diffview").setup({
+            keymaps = {
+                file_history_panel = {
+                    {
+                        "n",
+                        "<leader>gl",
+                        actions.open_in_diffview,
+                        { desc = "Open the entry under the cursor in a diffview" },
+                    },
+                },
+            },
+        })
+        vim.keymap.set("n", "<leader>gl", ":DiffviewFileHistory %<CR>", {})
+        vim.keymap.set("n", "<leader>gv", ":DiffviewOpen<CR>", {})
+    end,
 }
