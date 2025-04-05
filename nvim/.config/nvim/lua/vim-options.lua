bf8451be8f46fbdbf37173073e0d6d75b9c4c0c0
mikeymclellan <mikey@mclellan.org.nz>
2025-04-06 07:30:13 +1200

    Add path copy command

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -16,7 +16,7 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
--- Cetre the screen when jumping
+-- Centre the screen when jumping
 vim.keymap.set('n', '<c-f>', '<c-f>zz')
 vim.keymap.set('n', '<c-b>', '<c-b>zz')
 vim.keymap.set('n', 'n', 'nzz')
@@ -26,8 +26,9 @@ vim.keymap.set('n', '<c-t>', '<c-t>zz')
 -- navigate buffers with Tab
 -- vim.keymap.set('n', '<tab>', ':bnext<CR>')
 -- vim.keymap.set('n', '<s-tab>', ':bprevious<CR>')
+vim.keymap.set("n", '<leader>abd', ':%bd|e#<CR>', {}) -- Close all buffers except the current one
 
--- for some reason I need to mapp these here rather than in the gitsigns config :thinking-face:
+-- for some reason I need to map these here rather than in the gitsigns config :thinking-face:
 vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
 vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
 vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", {})
@@ -35,5 +36,13 @@ vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
 vim.keymap.set("n", "<leader>q", ":tabclose<CR>", {})
 
 vim.keymap.set('v', '<leader>y', '"*y')
+
+-- Copy current buffer's filename to system clipboard
+vim.keymap.set('n', '<leader>cn', function()
+    local filename = vim.fn.expand('%:p')
+    vim.fn.setreg('+', filename)
+    print('Copied filename to clipboard: ' .. filename)
+end)
+
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
